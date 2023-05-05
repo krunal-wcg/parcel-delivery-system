ad1bd603d061329ec5bec8aa097a602fcf8841db
krunal-wcg <krunal.b@webcodegenie.com>
2023-05-05 19:31:24 +0530

    automation done

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,29 +1,21 @@
 import React, { useEffect } from "react";
 import "./home.css";
-import { pakages } from "./data";
+import { pakages, truckDetails } from "./data";
 // import { Button, - } from "react-bootstrap";
-import { MDBProgress, MDBProgressBar } from "mdb-react-ui-kit";
+import { MDBBtn, MDBIcon, MDBProgress, MDBProgressBar } from "mdb-react-ui-kit";
 // import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { useState } from "react";
 
 
-const percentage = (x,y)=> (x*100 /y)
-  
-
-
+const percentage = (x, y) => (x * 100) / y;
 const HomePage = () => {
-  const [pakageList, setPakageList] = useState(pakages);  
-
-  /*TODO :
-  * change truck from static to dynamic 
-  * for that set image location to the truck
-  * get a map function in the it    
-  */
-  const [truck, setTruck] = useState([
-    { type: "cargo", parcel: [], capacity:1000,load:0},
-    { type: "delivery", parcel: [] , capacity:750,load:0},
-    { type: "vehicle", parcel: [] , capacity:500,load:0},
-  ]);
+  const [pakageList, setPakageList] = useState(pakages);
+  const [click, setclick] = useState("");
+  const [truck, setTruck] = useState(
+    truckDetails.map((oneTruck) => {
+      return { ...oneTruck, parcel: [], load: 0 };
+    })
+  );
 
   function drag(ev, item) {
     /* set the value that we drag   */
@@ -32,58 +24,123 @@ const HomePage = () => {
     ev.dataTransfer.setData("weight", item.weight);
   }
 
-
   function drop(ev) {
+    if (ev.dataTransfer.getData("id")) {
 
-    const truckType = ev.target.dataset.type;  // set truck type from the HTML atribute data-type
+      ev.preventDefault();
+
+      const truckType = ev.target.dataset.type; // set truck type from the HTML atribute data-type
 
       //set drag value to drop
-    const dropValue = {
-      name: ev.dataTransfer.getData("name"),
-      id: ev.dataTransfer.getData("id"),
-      weight: ev.dataTransfer.getData("weight")
-    };
-    console.log(truck[0].parcel);
-    truck.map((el)=>{
-      /*
-      *  check truck type and if it is full or not
-      *   set the truck parcel and load
-      */
-      if(el.type===truckType && el.load+(dropValue.weight*1) < el.capacity ){
-        setTruck(
-          truck.map((vehicle) => {
-            if (vehicle.type === truckType) {
-              return {
-                ...vehicle,
-                parcel: [...vehicle.parcel, dropValue],
-                load:vehicle.load+(dropValue.weight*1)
+      const dropValue = {
+        name: ev.dataTransfer.getData("name"),
+        id: ev.dataTransfer.getData("id"),
+        weight: ev.dataTransfer.getData("weight"),
+      };
+      console.log(truck[0].parcel);
+      truck.map((el) => {
+        /*
+        *  check truck type and if it is full or not
+        *   set the truck parcel and load
+        */
+        if (
+          el.type === truckType &&
+          el.load + dropValue.weight * 1 <= el.capacity
+        ) {
+          setTruck(
+            truck.map((vehicle) => {
+              if (vehicle.type === truckType) {
+                return {
+                  ...vehicle,
+                  parcel: [...vehicle.parcel, dropValue],
+                  load: vehicle.load + dropValue.weight * 1,
+                };
               }
-            }
-            return vehicle
-          })
-        );
+              return vehicle;
+            })
+          );
           //remove the data which one is drop
-         setPakageList(pakageList.filter((item) => item.id != dropValue.id));
+          setPakageList(pakageList.filter((item) => item.id != dropValue.id));
+        }
+      });
+    }
 
-      }
-
-    })
- 
-
-
-    ev.preventDefault();
+    console.log(truck);
   }
   function allowDrop(ev) {
     ev.preventDefault();
   }
 
+  // console.log(click);
+  const handleClick = (name) => {
+    setclick(name);
+  };
 
+
+  const handleRemoveTruck = (pakage, number) => {
+    console.log(pakage, number);
+    setTruck(truck.map((single) => {
+      if (single.truckNumber === number) {
+        console.log(single);
+        return {
+          ...single,
+          parcel: single.parcel.filter((item) => item.id != pakage.id),
+          load: single.load - pakage.weight
+        }
+      }
+      return single
+    }))
+    setPakageList([...pakageList, pakage])
+  }
+
+  // const remove = (item,box)=>{
+  //   setPakageList(pakageList =>pakageList.filter((item) => item.id != box.id));
+  // }
+
+  useEffect(() => {
+    
+   
+  }, [truck])
+  
+
+  const handleAll = () => {
+    let value = truck.slice(0).reverse()
+    // console.log(box.id);
+    value.map((single) =>{
+      pakageList.map((box) => {
+        // box.weight < single.minWeight && setTruck(truck=>truck.map((one)=>one.truckNumber===single.truckNumber ? {...one,minWeight:0}:one))
+        setTruck(truck => truck.map((item) => {
+          if (single.truckNumber === item.truckNumber) {
+            
+            if ((item.maxWeight >= box.weight * 1) && (item.load + box.weight * 1 <= item.capacity)/*  && (box.weight>item.minWeight) */) {
+              setPakageList(pakageList =>pakageList.filter((item) => item.id != box.id));
+              // console.log(pakageList,'================');
+              return {
+                ...item,
+                parcel: [...item.parcel, box],
+                load: item.load + (box.weight * 1)
+              }
+            }
+          }
+          return item;
+          
+        }))
+        // console.log(">>>>>>>>>>>>>>>>>>>>>.",pakageList);
+
+      }
+      )
+    })
+  }
+  console.log(truck);
 
   return (
     <>
       <div className="home_container">
         <aside className="left_bar">
-          {pakageList.map((item, index) => (
+          <div className="d-grid ">
+            <MDBBtn onClick={handleAll}> ADD ALL ...</MDBBtn>
+          </div>
+          {pakageList.map((item) => (
             <div
               key={item.id}
               className="pakeges_aside"
@@ -98,117 +155,91 @@ const HomePage = () => {
           ))}
         </aside>
         <main className="right_bar">
-          <div
-            id="1"
-            style={{ position: "relative", width: "350px" }}
-            onDrop={(e) => {
-              drop(e);
-            }}
-            onDragOver={(e) => {
-              allowDrop(e);
-            }} 
-          >
-            <MDBProgress height="275" className="mb-5">
-              <MDBProgressBar
-                striped
-                animated
-                bgColor="danger"
-                // width={uploadProgress}
-                width={percentage(truck[0].load,truck[0].capacity)}
-                valuemin={0}
-                valuemax={100}
-              >
-                {" "}
-              </MDBProgressBar>
-            </MDBProgress>
-            <div>
-              {" "}
-              <img
-                src="https://www.svgrepo.com/show/25233/delivery-truck.svg"
-                alt="cargo"
-                data-type="cargo"
-                style={{ width: "100%", position: "absolute", top: "0" }}
-              />{" "}
-            </div>
-          </div>
-
-          <div
-            style={{
-              position: "relative",
-              width: "350px",
-            }} /* ref={drop} data-testid="dustbin" */
-            onDrop={(e) => {
-              drop(e);
-            }}
-            onDragOver={(e) => {
-              allowDrop(e);
-            }}
-          >
-            <MDBProgress height="275" className="mb-5">
-              <MDBProgressBar
-                striped
-                animated
-                bgColor="danger"
-                // width={uploadProgress}
-                width={percentage(truck[1].load,truck[2].capacity)}
-                valuemin={0}
-                valuemax={100}
-              >
-                {" "}
-              </MDBProgressBar>
-            </MDBProgress>
-            <div>
-              {" "}
-              <img
-                src="https://cdn.onlinewebfonts.com/svg/img_10551.png"
-                data-type="delivery"
-                alt="delivery"
-                style={{ width: "100%", position: "absolute", top: "0" }}
-              />{" "}
-            </div>
-          </div>
-
-          <div
-            style={{
-              position: "relative",
-              width: "350px",
-            }} /*  ref={drop} data-testid="dustbin" */
-            onDrop={(e) => {
-              drop(e);
-            }}
-            onDragOver={(e) => {
-              allowDrop(e);
-            }}
-          >
-            <MDBProgress height="275" className="mb-5">
-              <MDBProgressBar
-                striped
-                animated
-                bgColor="danger"
-                // width={uploadProgress}
-                width={percentage(truck[2].load,truck[2].capacity)}
-                valuemin={0}
-                valuemax={100}
-              >
-                {" "}
-              </MDBProgressBar>
-            </MDBProgress>
-            <div>
-              {" "}
-              <img
-                src="https://www.iconpacks.net/icons/1/free-truck-icon-1058-thumb.png"
-                alt="vehicle"
-                data-type="vehicle"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  position: "absolute",
-                  top: "0",
+          {truck.map((singleTruck, index) => (
+            <>
+              <div
+                key={index}
+                onClick={() => {
+                  handleClick(singleTruck.type);
                 }}
-              />{" "}
-            </div>
-          </div>
+                id="1"
+                style={{ position: "relative", width: "350px" }}
+                onDrop={(e) => {
+                  drop(e);
+                }}
+                onDragOver={(e) => {
+                  allowDrop(e);
+                }}
+              >
+                <MDBProgress height="275" className="mb-5">
+                  <MDBProgressBar
+                    striped
+                    animated
+                    bgColor="danger"
+                    // width={uploadProgress}
+                    width={percentage(singleTruck.load, singleTruck.capacity)}
+                    valuemin={0}
+                    valuemax={100}
+                  >
+                    {" "}
+                  </MDBProgressBar>
+                </MDBProgress>
+                <div>
+                  {" "}
+                  <img
+                    src={singleTruck.img}
+                    alt={singleTruck.type}
+                    data-type={singleTruck.type}
+                    style={{ width: "100%", position: "absolute", top: "0" }}
+                  />{" "}
+                </div>
+              </div>
+            </>
+          ))}
         </main>
+        <div className="details_bar">
+          {/* <h1>hello</h1> */}
+
+          {click &&
+            truck.map(
+              (item) =>
+                item.type === click && (
+                  <>
+                    {/* <div style={{width:'100%'}}> */}
+
+                    <img src={item.img} className="details-image " alt={item.type} />
+                    {/* </div> */}
+                    <h2>{item.truckNumber}</h2>
+
+                    <table style={{ width: '100%' }}>
+                      <thead>
+                        <tr>
+                          <th className="list-td" >Parcel-Id</th>
+                          <th className="list-td" >Name</th>
+                          <th className="list-td" >Parcel weight</th>
+                          <th className="list-td" >Remove </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        {item.parcel.map((pakage) => (
+                          <tr className=" details-list">
+
+                            <td className="list-td">{pakage.id}</td>
+                            <td className="list-td">{pakage.name}</td>
+                            <td className="list-td">{pakage.weight} </td>
+                            <td className="list-td">
+                              <MDBBtn className="bg-danger" onClick={() => handleRemoveTruck(pakage, item.truckNumber)}> <MDBIcon fas icon="trash" /></MDBBtn>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+
+                  </>
+                )
+            )}
+        </div>
       </div>
       {/* </DragDropContext> */}
     </>
--- src/data.js
+++ src/data.js
@@ -1,72 +1,108 @@
-export  const pakages = [
+export const pakages = [
+    { id: 1, name: 'Box', weight: 10 },
+    { id: 2, name: 'Crate', weight: 20 },
+    { id: 3, name: 'Barrel', weight: 30 },
+    { id: 4, name: 'Bag', weight: 5 },
+    { id: 5, name: 'Pallet', weight: 100 },
+    { id: 6, name: 'Drum', weight: 50 },
+    { id: 7, name: 'Canister', weight: 15 },
+    { id: 8, name: 'Bar', weight: 40 },
+    { id: 9, name: 'Tube', weight: 3 },
+    { id: 10, name: 'Sphere', weight: 25 },
+    { id: 11, name: 'Envelope', weight: 0.5 },
+    { id: 12, name: 'Bag', weight: 7 },
+    { id: 13, name: 'Crate', weight: 15 },
+    { id: 14, name: 'Cylinder', weight: 12 },
+    { id: 15, name: 'Box', weight: 8 },
+    { id: 16, name: 'Pallet', weight: 75 },
+    { id: 17, name: 'Bag', weight: 2 },
+    { id: 18, name: 'Drum', weight: 45 },
+    { id: 19, name: 'Canister', weight: 18 },
+    { id: 20, name: 'Bar', weight: 35 },
+    { id: 21, name: 'Sphere', weight: 20 },
+    { id: 22, name: 'Box', weight: 9 },
+    { id: 23, name: 'Crate', weight: 25 },
+    { id: 24, name: 'Barrel', weight: 35 },
+    { id: 25, name: 'Bag', weight: 6 },
+    { id: 26, name: 'Pallet', weight: 90 },
+    { id: 27, name: 'Drum', weight: 55 },
+    { id: 28, name: 'Canister', weight: 10 },
+    { id: 29, name: 'Bar', weight: 50 },
+    { id: 30, name: 'Tube', weight: 2 },
+    { id: 31, name: 'Sphere', weight: 30 },
+    { id: 32, name: 'Envelope', weight: 0.3 },
+    { id: 33, name: 'Box', weight: 12 },
+    { id: 34, name: 'Crate', weight: 18 },
+    { id: 35, name: 'Cylinder', weight: 15 },
+    { id: 36, name: 'Bag', weight: 4 },
+    { id: 37, name: 'Pallet', weight: 80 },
+    { id: 38, name: 'Drum', weight: 60 },
+    { id: 39, name: 'Canister', weight: 20 },
+    { id: 40, name: 'Bar', weight: 45 },
+    { id: 41, name: 'Sphere', weight: 18 },
+    { id: 42, name: 'Box', weight: 6 },
+    {id: 43, name: 'Book', weight: 2},
+    {id: 44, name: 'Pen', weight: 0.5},
+    {id: 45, name: 'Laptop', weight: 3},
+    {id: 46, name: 'Mouse', weight: 0.2},
+    {id: 47, name: 'Keyboard', weight: 1},
+    {id: 48, name: 'Phone Charger', weight: 0.1},
+    {id: 49, name: 'Headphones', weight: 0.5},
+    {id: 50, name: 'Sunglasses', weight: 0.2},
+    {id: 51, name: 'Water Bottle', weight: 0.5},
+    {id: 52, name: 'Wallet', weight: 0.2},
+    {id: 53, name: 'Umbrella', weight: 0.8},
+    {id: 54, name: 'T-Shirt', weight: 0.3},
+    {id: 55, name: 'Shorts', weight: 0.4},
+    {id: 56, name: 'Sneakers', weight: 0.7},
+    {id: 57, name: 'Hat', weight: 0.2},
+    {id: 58, name: 'Socks', weight: 0.1},
+    {id: 59, name: 'Toothbrush', weight: 0.1},
+    {id: 60, name: 'Toothpaste', weight: 0.2},
+    {id: 61, name: 'Shampoo', weight: 0.4},
+    {id: 62, name: 'Conditioner', weight: 0.4},
+    {id: 63, name: 'Soap', weight: 0.2},
+    {id: 64, name: 'Towel', weight: 0.6},
+    {id: 65, name: 'Glasses', weight: 0.3},
+    {id: 66, name: 'Medicine', weight: 0.2},
+    {id: 67, name: 'Lotion', weight: 0.3},
+    {id: 68, name: 'Deodorant', weight: 0.1},
+    {id: 69, name: 'Sunscreen', weight: 0.4},
+    {id: 70, name: 'Hand Sanitizer', weight: 0.2},
+    {id: 71, name: 'Mask', weight: 0.1},
+    {id: 72, name: 'Gloves', weight: 0.1},
+    {id: 73, name: 'Earplugs', weight: 0.1},
+    {id: 74, name: 'Pillow', weight: 0.8},
+    {id: 75, name: 'Blanket', weight: 0.7},
+    {id: 76, name: 'Lip Balm', weight: 0.1},
+    {id: 77, name: 'Snacks', weight: 0.6},
+    {id: 78, name: 'Gum', weight: 0.1},
+    {id: 79, name: 'Chapstick', weight: 0.1}
+  ]
+
+export const truckDetails = [
     {
-        id:'1'
-        ,name:'wheet'
-        ,weight:'20'
+        type: "cargo",
+        truckNumber: "GJ-01-K1470",
+        capacity: 1000,
+        img: "https://www.svgrepo.com/show/25233/delivery-truck.svg",
+        maxWeight: 1000,
+        minWeight:10
+    }, // parcel: [],load:0
+    {
+        type: "delivery",
+        truckNumber: "GJ-01-P1840",
+        capacity: 750,
+        img: "https://www.iconpacks.net/icons/1/free-truck-icon-1058-thumb.png",
+        maxWeight: 10,
+        minWeight:1
     },
     {
-        id:'2'
-        ,name:'rice'
-        ,weight:'30'
+        type: "vehicle",
+        truckNumber: "GJ-01-M5470",
+        capacity: 500,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Linecons_small-truck.svg/2048px-Linecons_small-truck.svg.png",
+        maxWeight: 1,
+        minWeight:0
     },
-    {
-        id:'3'
-        ,name:'friege'
-        ,weight:'30'
-    },
-    {
-        id:'4'
-        ,name:'book'
-        ,weight:'40'
-    },
-    {
-        id:'5'
-        ,name:'book'
-        ,weight:'15'
-    },
-    {
-        id:'6'
-        ,name:'book'
-        ,weight:'2'
-    },
-    {
-        id:'7'
-        ,name:'book'
-        ,weight:'8'
-    },
-    {
-        id:'8'
-        ,name:'book'
-        ,weight:'90'
-    },
-    {
-        id:'9'
-        ,name:'book'
-        ,weight:'69'
-    },
-    {
-        id:'10'
-        ,name:'book'
-        ,weight:'250'
-    },
-    {
-        id:'11'
-        ,name:'book'
-        ,weight:'100'
-    },
-    {
-        id:'12'
-        ,name:'book'
-        ,weight:'165'
-    },
-    {
-        id:'13'
-        ,name:'book'
-        ,weight:'250'
-    },
-    {
-        id:'14'
-        ,name:'book'
-        ,weight:'300'
-    },
-]
+];
